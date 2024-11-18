93a8745140bd088b68f293e0aba9b0ab54a12fe0
aniqaznaff <aniqazzanafisahn@gmail.com>
2024-11-18 20:18:15 +0700

    Add files via upload

---
+++ src/services/getAllProducts.js
@@ -0,0 +1,81 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'NB-574-unisex-001',
+      slug: 'new-balance-574-unisex',
+      name: "New Balance 574 Legacy Unisex Sneakers",
+      category: 'unisex\'s Shoe',
+      price: 1_799_000,
+      stock: 100,
+      imageUrl: '/assets/images/01-NEW-BALANCE-574-Grey.jpg',
+      description: 'Desain hybrid road/trail dibangun di atas siluet balap sempit generasi sebelumnya yang lebih lebar, 574 menawarkan perpaduan unik yang unik antara baru, berbeda, tidak rumit, kokoh, tahan lama, dan nyaman yang diadopsi sebagai sepatu di seluruh dunia.'
+    },
+
+    {
+      id: 'NB-2002-boys-002',
+      slug: 'new-balance-force-2002-boys',
+      name: "New Balance 2002 Hook & Loop Boys Sneakers Shoes",
+      category: 'boy\'s Shoe',
+      price: 1_299_000,
+      stock: 50,
+      imageUrl: `/assets/images/01-NEW-BALANCE-2002-Beige.jpg`,
+      description: `Terinspirasi dari gaya sepatu lari tahun 2000-an, sepatu klasik untuk anak ini didesain untuk dapat menemani kakinya yang aktif.`
+    },
+
+    {
+      id: 'NB-327-WOMEN-003',
+      slug: 'new-balance-force-327-womens',
+      name: "New Balance 327 Women's Sneakers Shoes - White",
+      category: 'Women\'s Shoe',
+      price: 1_599_000,
+      stock: 0,
+      imageUrl: `/assets/images/01-NEW-BALANCE-327-White.jpg`,
+      description: `Sepatu 327 hadir sebagai salah satu pelopor pada tahun 70-an yang menyajikan sebuah desain klasik yang dibentuk ulang dan dipadukan dengan tampilan bernuansa kontemporer.`
+    },
+
+    {
+      id: 'NB-550-MEN-004',
+      slug: 'new-balance-force-550-mens',
+      name: "'New Balance 550 Men's Sneakers - White/Black",
+      category: 'Men\'s Shoe',
+      price: 1_999_000,
+      stock: 10,
+      imageUrl: `/assets/images/01-NEW-BALANCE-550-White Black.jpg`,
+      description: `Awalnya dipakai oleh para profesional, 550 yang baru memberi penghormatan kepada yang asli 1989 dengan detail klasik yang mengingatkan pada zaman itu - sederhana, bersih, dan sesuai dengan warisannya.`
+    },
+
+    {
+      id: 'NB-1906-WOMEN-005',
+      slug: 'new-balance-force-1906-unisex',
+      name: "New Balance 1906 Unisex Sneakers Shoes - Beige/Green",
+      category: 'Unisex\'s Shoe',
+      price: 2_599_000,
+      stock: 30,
+      imageUrl: `/assets/images/01-NEW-BALANCE-1906-Beige Green.jpg`,
+      description: `Sepatu lari ini dibuat dengan fitur kenyamanan serta teknologi premium guna mengimbangi gaya hidup aktifmu.`
+    },
+
+    {
+      id: 'NB-2002-MEN-006',
+      slug: 'new-balance-force-2002-mens',
+      name: "New Balance 2002 Men's Sneakers Shoes - Olive",
+      category: 'Men\'s Shoe',
+      price: 2_799_000,
+      stock: 20,
+      imageUrl: `/assets/images/01-NEW-BALANCE-2002-Olive.jpg`,
+      description: `Sepatu 327 hadir sebagai salah satu pelopor pada tahun 70-an yang menyajikan sebuah desain klasik yang dibentuk ulang dan dipadukan dengan tampilan bernuansa kontemporer.`
+    },
+
+    {
+      id: 'NB-1906-MEN-007',
+      slug: 'new-balance-force-1906-mens',
+      name: "New Balance 1906 Men's Sneakers Shoes - Grey",
+      category: 'Men\'s Shoe',
+      price: 2_599_000,
+      stock: 100,
+      imageUrl: `/assets/images/01-NEW-BALANCE-1906-Grey.jpg`,
+      description: `Sepatu lari ini didesain untuk memberikan kenyamanan dan teknologi premium untuk menunjang gaya hidup aktif Anda.`
+    },
+
+  ]
+}
